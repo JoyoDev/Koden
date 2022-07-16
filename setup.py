76f53a4d5591bc6b51fa123c080ba88e9aac7a59
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2022-07-16 18:01:52 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+description = "Simple Docker container orchestration tool"
+long_description = open("README.rst").read()
+url = "https://github.com/JoyoDev/koden"
+
+
+setup(
+    name="koden",
+    version="0.0.1",
+    author="Aleksandar Milanovic",
+    author_email="joyo.development@gmail.com",
+    description=description,
+    license="MIT",
+    keywords="docker container orchestration",
+    url=url,
+    packages=find_packages(),
+    long_description=long_description,
+    classifiers=[],
+)
