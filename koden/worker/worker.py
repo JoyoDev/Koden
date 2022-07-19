e5ecd9b6b65b6a2167505485e9c83ad0bb7880c3
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2022-07-19 14:11:04 +0200

    Implement start and stop task methods for worker

--- koden/worker/worker.py
+++ koden/worker/worker.py
@@ -1,7 +1,15 @@
 from typing import Dict
+from koden.task.state import State
 from koden.task.task import Task
+from koden.task.config import Config
+from koden.task.docker_client import DockerResult, DockerClient
 from uuid import UUID
 from queue import Queue
+from datetime import datetime
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelname)s:%(message)s')
+logger = logging.getLogger(__name__)
 
 
 class Worker:
@@ -18,11 +26,40 @@ class Worker:
     def collect_stats(self):
         pass
 
+    def add_task(self, task: Task):
+        self.queue.put(task)
+
     def run_task(self):
         pass
 
-    def start_task(self):
-        pass
+    def start_task(self, task: Task) -> DockerResult:
+        config = Config(task=task)
+        d = DockerClient(task_config=config)
 
-    def stop_task(self):
-        pass
+        result = d.run()
+        if result.error is not None:
+            logger.error(f"Error starting task {task.id} with error {result.error}")
+            task.state = State.FAILED
+            return result
+
+        task.container_id = result.container_id
+        task.state = State.RUNNING
+        self.db[task.id] = task
+
+        return result
+
+    def stop_task(self, task: Task) -> DockerResult:
+        config = Config(task=task)
+        d = DockerClient(task_config=config)
+
+        result = d.stop(task.container_id)
+        if result.error is not None:
+            logger.error(f"Error stopping container {task.container_id} with error {result.error}")
+            return result
+
+        task.finish_time = datetime.now()
+        task.state = State.COMPLETED
+        self.db[task.id] = task
+        logger.info(f"Stopped and removed container {task.container_id} for task {task.id}")
+
+        return result
